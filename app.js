efbe7e392031b2ef67801993037eaa679280dcb8
Daniel Rakaczki <daniel.rakaczki@gmail.com>
2021-09-10 14:19:26 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/* const lis = document.querySelectorAll('li');
+lis.forEach((li, index) => {
+  if (index > 0) {
+    if (li.textContent === '') {
+      li.textContent = '-';
+    };
+  };
+}); */
