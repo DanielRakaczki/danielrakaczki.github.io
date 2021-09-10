bfeb6b5473320c004c042783004a8b78f52077dd
Daniel Rakaczki <daniel.rakaczki@gmail.com>
2021-09-10 23:29:15 +0200

    Added some responsiveness and website only shows todays classes

--- app.js
+++ app.js
@@ -6,3 +6,31 @@ lis.forEach((li, index) => {
     };
   };
 }); */
+
+const media = window.matchMedia('(max-width: 769px)');
+const days = document.querySelectorAll('.day');
+
+if (media.matches) { document.addEventListener('DOMContentLoaded', setDay()) }
+
+function setDay() {
+  const time = new Date();
+  switch (time.getDay()) {
+    case 1: removeDays(1); break;
+    case 2: removeDays(2); break;
+    case 3: removeDays(3); break;
+    case 4: removeDays(4); break;
+    case 5: removeDays(5); break;
+    case 6: removeDays(6); break;
+    case 0: removeDays(0); break;
+  }
+
+  function removeDays(i) {
+    if (i > 0 && i < 6) {
+      days.forEach((day, index) => {
+        if (index !== i - 1) {
+          day.style.display = 'none';
+        }
+      });
+    }
+  }
+}
